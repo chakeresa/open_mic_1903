660d9c7ec8378488e246cc23f8674eb90997074e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-20 09:59:59 -0600

    Add OpenMic initialize method and corresponding OpenMicTest tests

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -2,4 +2,9 @@ require './lib/joke'
 require './lib/user'
 
 class OpenMic
+  attr_reader :location, :date
+  def initialize(show)
+    @location = show[:location]
+    @date = show[:date]
+  end
 end
--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -3,4 +3,23 @@ require 'minitest/pride'
 require './lib/open_mic'
 
 class OpenMicTest < Minitest::Test
+  def test_it_exists
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    assert_instance_of OpenMic, open_mic
+  end
+
+  def test_it_initializes_with_hash_of_location_and_date
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    assert_equal "Comedy Works", open_mic.location
+    assert_equal "11-20-18", open_mic.date
+  end
+
+  def test_it_initializes_with_different_location_and_date
+    open_mic = OpenMic.new({location: "The Comedy Store", date: "3-7-19"})
+
+    assert_equal "The Comedy Store", open_mic.location
+    assert_equal "3-7-19", open_mic.date
+  end
 end
