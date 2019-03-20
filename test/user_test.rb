684846b26270800c6a89402b7cb8035ff09db3c0
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-20 09:25:22 -0600

    Test starting values of name and jokes for User

--- test/user_test.rb
+++ test/user_test.rb
@@ -9,4 +9,16 @@ class UserTest < Minitest::Test
 
     assert_instance_of User, sal
   end
+
+  def test_it_initializes_with_name
+    sal = User.new("Sal")
+
+    assert_equal "Sal", sal.name
+  end
+
+  def test_it_starts_with_an_empty_ary_of_jokes
+    sal = User.new("Sal")
+
+    assert_equal [], sal.jokes
+  end
 end
